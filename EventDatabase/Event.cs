6e894061c376e79c1865bbb56484a5c71f59b8b2
glopes <glopes@localhost>
2013-01-24 21:44:32 +0000

    Added first version of EventDatabase, a simple event logger for experiments.

---
+++ EventDatabase/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventDatabase
+{
+    public class Event
+    {
+        public Event()
+        {
+            Timestamp = DateTimeOffset.Now;
+        }
+
+        public DateTimeOffset Timestamp { get; set; }
+
+        public EventType EventType { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2}", Timestamp, EventType, Value);
+        }
+
+        public static Event Parse(string s)
+        {
+            var parameters = s.Split(',');
+            var evt = new Event();
+            if (parameters.Length > 0) evt.Timestamp = DateTimeOffset.Parse(parameters[0]);
+            if (parameters.Length > 1) evt.EventType = (EventType)Enum.Parse(typeof(EventType), parameters[1]);
+            if (parameters.Length > 2) evt.Value = parameters[2];
+            return evt;
+        }
+    }
+}
---
+++ EventDatabase/EventWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace EventDatabase
+{
+    public static class EventWriter
+    {
+        public static void Write(TextWriter writer, Event evt)
+        {
+            writer.WriteLine();
+        }
+
+        public static Event ReadEvent(TextReader reader)
+        {
+            var eventText = reader.ReadLine();
+            var parameters = eventText.Split(',');
+            var evt = new Event();
+            if (parameters.Length > 0) evt.Timestamp = DateTimeOffset.Parse(parameters[0]);
+            if (parameters.Length > 1) evt.EventType = (EventType)Enum.Parse(typeof(EventType), parameters[1]);
+            if (parameters.Length > 2) evt.Value = parameters[2];
+            return evt;
+        }
+    }
+}
